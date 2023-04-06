cc32d152f6ef14a31fef7672b3e938fa9fa214b5
kelvintaywl <kelvintaywl@gmail.com>
2023-04-06 11:35:27 +0900

    chore: rm terraform stuff

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "telegraf_docker_image" {
-  description = "Docker image:tag for Telegraf"
-  # NOTE: this image is only available for linux/amd64
-  # In other words, this image is not usable on Mac M1
-  # See https://hub.docker.com/_/telegraf/tags?page=1&name=1.17-alpine
-  default = "telegraf:1.17-alpine"
-}
